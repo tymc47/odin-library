2115cabfa2a27d9752754019e36e0fc83df30ce7
Terry Mak <fmak.1536@gmail.com>
2022-09-23 11:55:19 +0800

    save with prettier

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let myLibrary = [];
 
 class book {
-  constructor (name, author, page, status){
+  constructor(name, author, page, status) {
     this.name = name;
     this.author = author;
     this.page = page;
@@ -9,42 +9,50 @@ class book {
   }
 
   get info() {
-    return this.name + " by " + this.author + ", " + this.page + "pages, " + (this.read ? "read" : "not read yet");
+    return (
+      this.name +
+      " by " +
+      this.author +
+      ", " +
+      this.page +
+      "pages, " +
+      (this.read ? "read" : "not read yet")
+    );
   }
 
   changeReadStatus() {
-    this.read ? this.read = false : this.read = true;
+    this.read ? (this.read = false) : (this.read = true);
   }
 }
 
-function addBookToLibrary(){
-  let name = document.getElementById('bookTitle').value;
-  let author = document.getElementById('bookAuthor').value;
-  let page = document.getElementById('bookPages').value;
-  let read = document.getElementById('bookRead').checked;
+function addBookToLibrary() {
+  let name = document.getElementById("bookTitle").value;
+  let author = document.getElementById("bookAuthor").value;
+  let page = document.getElementById("bookPages").value;
+  let read = document.getElementById("bookRead").checked;
 
   if (!name || !author || !page) {
-    alert("Please enter all the details")
+    alert("Please enter all the details");
     return;
   }
 
-  document.querySelector('.addbook').reset();
+  document.querySelector(".addbook").reset();
   let record = new book(name, author, page, read);
   myLibrary.push(record);
   sortData();
   refreshTable();
 }
 
-function createBtn(type, index){
-  let btn = document.createElement("button")
+function createBtn(type, index) {
+  let btn = document.createElement("button");
   btn.id = index;
 
-  if (type == "delete"){
-    btn.innerHTML = "Delete"
-    btn.setAttribute("onclick", "delBook(event)")
+  if (type == "delete") {
+    btn.innerHTML = "Delete";
+    btn.setAttribute("onclick", "delBook(event)");
   } else {
-    btn.innerHTML = "Read/Unread"
-    btn.setAttribute("onclick", "changeStatus(event)")
+    btn.innerHTML = "Read/Unread";
+    btn.setAttribute("onclick", "changeStatus(event)");
   }
   return btn;
 }
@@ -52,9 +60,11 @@ function createBtn(type, index){
 function delBook(e) {
   const bookId = e.currentTarget.id;
 
-  const action = confirm("Are you sure you want to delete " + myLibrary[bookId].name)
-  if (action){
-    myLibrary.splice(bookId, 1); 
+  const action = confirm(
+    "Are you sure you want to delete " + myLibrary[bookId].name
+  );
+  if (action) {
+    myLibrary.splice(bookId, 1);
     refreshTable();
   }
 }
@@ -65,40 +75,66 @@ function changeStatus(e) {
   refreshTable();
 }
 
-
-let record1 = new book("Harry Potter and the Philosopher's Stone", "J. K. Rowling", 223, false);
+let record1 = new book(
+  "Harry Potter and the Philosopher's Stone",
+  "J. K. Rowling",
+  223,
+  false
+);
 myLibrary.push(record1);
 
-let record2 = new book("Harry Potter and the Chamber of Secrets", "J. K. Rowling", 251, false);
+let record2 = new book(
+  "Harry Potter and the Chamber of Secrets",
+  "J. K. Rowling",
+  251,
+  false
+);
 myLibrary.push(record2);
 
-let record3 = new book("Harry Potter and the Prisoner of Azkaban", "J. K. Rowling", 317, false);
+let record3 = new book(
+  "Harry Potter and the Prisoner of Azkaban",
+  "J. K. Rowling",
+  317,
+  false
+);
 myLibrary.push(record3);
 
-function refreshTable(){
-  const tableBody = document.querySelector('tbody');
+function refreshTable() {
+  const tableBody = document.querySelector("tbody");
   tableBody.innerHTML = "";
 
-  myLibrary.forEach(record => {
+  myLibrary.forEach((record) => {
     const delBtn = createBtn("delete", myLibrary.indexOf(record));
     const statusBtn = createBtn("change", myLibrary.indexOf(record));
     let bookStatus = false;
 
-    if (record.read) {bookStatus = "Yes"}
-    else {bookStatus = "No"};
+    if (record.read) {
+      bookStatus = "Yes";
+    } else {
+      bookStatus = "No";
+    }
 
     let row = tableBody.insertRow(-1);
-    row.innerHTML = "<td>" + record.name + "</td>" 
-                    + "<td>" + record.author + "</td>" 
-                    + "<td>" + record.page + "</td>" 
-                    + "<td>" + bookStatus + "</td>";
-    row.insertCell(-1).append(statusBtn) ;
-    row.insertCell(-1).append(delBtn)
-  })
+    row.innerHTML =
+      "<td>" +
+      record.name +
+      "</td>" +
+      "<td>" +
+      record.author +
+      "</td>" +
+      "<td>" +
+      record.page +
+      "</td>" +
+      "<td>" +
+      bookStatus +
+      "</td>";
+    row.insertCell(-1).append(statusBtn);
+    row.insertCell(-1).append(delBtn);
+  });
 }
 
-function sortData(){
-  myLibrary.sort((book1, book2) =>  (book1.name).localeCompare(book2.name));
+function sortData() {
+  myLibrary.sort((book1, book2) => book1.name.localeCompare(book2.name));
 }
 
 refreshTable();
